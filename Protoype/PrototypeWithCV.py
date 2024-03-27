6825f38ef5900d8b40a3d2cb547df9cadae9813c
Jonas <54221977+Shaddowrunner414@users.noreply.github.com>
2024-03-27 20:19:13 +0100

    added readme + fixes
    set image path as relative to .py file
    added head-tracked middle_layer movement

--- Protoype/PrototypeWithCV.py
+++ Protoype/PrototypeWithCV.py
@@ -1,6 +1,7 @@
 import cv2
 import pygame
 import sys
+import os
 
 # Pygame und OpenCV initialisieren
 pygame.init()
@@ -10,18 +11,34 @@ cap = cv2.VideoCapture(0)  # 0 steht für die erste Kamera
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Fenstergröße und Farben definieren
-fenster_groesse = (640, 480)
+width = 1000
+heigth = 800
+fenster_groesse = (width, heigth)
 schwarz = (0, 0, 0)
 fenster = pygame.display.set_mode(fenster_groesse)
+half_x = width * 0.2
+half_y = heigth * 0.2
+
+#Pfad des aktuellen Skripts ermittlen
+script_dir = os.path.dirname(__file__)
+
+#Relative Pfade für die Bilder festlegen
+background_path = os.path.join(script_dir, "Background.png")
+forground_path = os.path.join(script_dir, "Foreground.png")
+middle_path = os.path.join(script_dir, "MiddleLayer.png")
 
 # Bilder laden
-hintergrund = pygame.image.load("Background.png").convert()
-vordergrund = pygame.image.load("Foreground.png").convert_alpha()
-zwischenebene = pygame.image.load("MiddleLayer.png").convert_alpha()  # Die dritte Ebene
+hintergrund = pygame.image.load(background_path).convert()
+vordergrund = pygame.image.load(forground_path).convert_alpha()
+zwischenebene = pygame.image.load(middle_path).convert_alpha()  # Die dritte Ebene
 
 
-# Anfangsposition des Vordergrundbildes
-x_vordergrund, y_vordergrund = 50, 100
+# Anfangsposition der Bilder
+x_vordergrund, y_vordergrund = half_x, half_y
+x_zwischenebene, y_zwischenebene = half_x, half_y
+
+#letze Erkannte Position
+lastknown_x, lastknown_y = 0, 0
 
 # Spiel-Schleife
 running = True
@@ -38,13 +55,24 @@ while running:
 
     # Erstes erkanntes Gesicht verwenden, um die Position zu aktualisieren
     for (x, y, w, h) in faces:
-        x_vordergrund = x - w // 2
-        y_vordergrund = y - h // 2
+        if lastknown_x !=0 and lastknown_y !=0:
+            x_diff = x - lastknown_x
+            y_diff = y - lastknown_y
+            
+            x_vordergrund += x_diff
+            y_vordergrund -= y_diff #diese Zeile auskommentieren, wenn kein vertikales Tracking / Bewegung gewünscht
+
+            # Bewege Middle Layer mit halber Geschwindigkeit
+            x_zwischenebene += x_diff // 3
+            y_zwischenebene -= y_diff // 3 #diese Zeile auskommentieren, wenn kein vertikales Tracking / Bewegung gewünscht
+
+        lastknown_x, lastknown_y = x, y
         break  # Nur das erste Gesicht verwenden
 
     # Hintergrund und Vordergrund zeichnen
     fenster.fill(schwarz)
     fenster.blit(hintergrund, (0, 0))
+    fenster.blit(zwischenebene, (x_zwischenebene, y_zwischenebene))
     fenster.blit(vordergrund, (x_vordergrund, y_vordergrund))
     
     # Fenster aktualisieren
---
+++ Readme.txt
@@ -0,0 +1,14 @@
+Prerequisites:
+Python:
+	https://www.python.org/downloads/
+Pip:
+	py -m ensurepip --upgrade
+	Download the script, from https://bootstrap.pypa.io/get-pip.py	
+	Open a terminal/command prompt, cd to the folder containing the get-pip.py file and run:
+	py get-pip.py
+
+Pip packages:
+	OpenCV:
+		pip install opencv-python (alternativ: pip3 install opencv-python)
+	Pygame:
+		pip install pygame
