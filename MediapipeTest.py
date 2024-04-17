54ea619772f14aa2d6bf7c3e5a3e35621c57c656
moritzpollich <moritzpollich0902@gmail.com>
2024-04-17 17:14:31 +0200

    Tagebucheintrag

--- MediapipeTest.py
+++ MediapipeTest.py
@@ -1,9 +1,14 @@
 import cv2
 import mediapipe as mp
+import pygame
 
 # Initialisiere Mediapipe Face Detection
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
+
+# Bildschirmauflösung auslesen
+info = pygame.display.Info()
+width, height = info.current_w, info.current_h
 
 # Starte die Webcam
 cap = cv2.VideoCapture(0)
@@ -16,21 +21,47 @@ with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detec
             continue
 
         # Konvertiere das Bild von BGR nach RGB
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
         # Verbessere die Leistung
-        image.flags.writeable = False
+        imageRGB.flags.writeable = False
         
         # Erkenne Gesichter
-        results = face_detection.process(image)
+        results = face_detection.process(imageRGB)
+
 
         # Zeichne die Gesichtsboxen
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        imageRGB.flags.writeable = True
+
+
+        # Koordinaten des Gesichts auslesen und motion smoothing anwenden
         if results.detections:
-            detection = results.detections[0]
-            mp_drawing.draw_detection(image, detection)
+            for detection in results.detections:
+                bbox = detection.location_data.relative_bounding_box
+                x_face_now = int(bbox.xmin * width)
+                y_face_now = int(bbox.ymin * height)
+                lastknown_x = (x_face_now + lastknown_x * 9) / 10
+                lastknown_y = (y_face_now + lastknown_y * 9) / 10
+        else:
+            lastknown_x = (lastknown_x * 9) / 10
+            lastknown_y = (lastknown_y * 9) / 10
+
+        print("x: ", x_face_now, "y: ", y_face_now)
+
+
+        #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.detections:
+            bestIdx = 0
+
+            for detection in results.detections:
+                #bestIdx finden
+                pass
+
+            mp_drawing.draw_detection(image, results.detections[bestIdx])
         
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
         # Zeige das Bild an
         cv2.imshow('Gesichtserkennung', image)
         if cv2.waitKey(5) & 0xFF == 27:  # Drücke ESC zum Beenden
