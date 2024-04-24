021e5a777484f108a2cfc48c7f8df292ba45205e
Zaphycs <domk1998@gmail.com>
2024-04-24 15:19:18 +0200

    Update Funktionen für Jonas

--- Protoype/GetSystemScaling.py
+++ Protoype/GetSystemScaling.py
@@ -22,3 +22,26 @@ horizontal_scale, vertical_scale = get_system_scaling()
 if horizontal_scale is not None and vertical_scale is not None:
     print("Horizontale Systemskalierung:", horizontal_scale)
     print("Vertikale Systemskalierung:", vertical_scale)
+
+
+layer1 = load_and_scale("background.png")
+layer2 = load_and_scale("hill.png", scale_factor=1.0)
+layer3 = load_and_scale("tree.png", scale_factor=1.0)
+layer4 = load_and_scale("fence.png", scale_factor=1.0)
+
+x_layer1, x_layer2, x_layer3, x_layer4 = x_starter, x_starter, x_starter, x_starter
+y_layer1, y_layer2, y_layer3, y_layer4 = y_starter, y_starter, y_starter, y_starter
+
+
+  x_layer2 = x_neu
+        y_layer2 = y_neu
+        x_layer3 = x_neu
+        y_layer3 = y_neu
+        x_layer4 = x_neu
+        y_layer4 = y_neu
+
+ window.fill(black)
+        window.blit(layer1, (0, 0))
+        window.blit(layer2, (x_layer2, y_layer2)) 
+        window.blit(layer3, (x_layer3, y_layer3))
+        window.blit(layer4, (x_layer4, y_layer4))
